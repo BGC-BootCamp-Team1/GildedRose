0610d298c7c6b2ec5c07f7ae6cc8fd1c1961280a
Sheng Liu <SLiu40@slb.com>
2024-10-30 19:07:36 +0800

    REFACTOR.to Strategy pattern

---
+++ GildedRose/Strategy/BackstagePassesUpdateStrategy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Strategy
+{
+    public class BackstagePassesUpdateStrategy : IUpdateStrategy
+    {
+        public void Update(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality++;
+                if (item.SellIn < 11 && item.Quality < 50)
+                {
+                    item.Quality++;
+                }
+                if (item.SellIn < 6 && item.Quality < 50)
+                {
+                    item.Quality++;
+                }
+            }
+            item.SellIn--;
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+            }
+        }
+    }
+}
