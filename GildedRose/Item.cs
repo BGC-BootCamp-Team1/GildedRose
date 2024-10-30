455059d28b8c74402650ee7192dde24b99bd4d2a
Anyan Zhao <AZhao4@slb.com>
2024-10-30 19:32:06 +0800

    refactor: remove duplication

--- GildedRose/Item.cs
+++ GildedRose/Item.cs
@@ -6,5 +6,17 @@ namespace GildedRose
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+
+        private const int QUALITY_MAX = 50;
+        private const int QUALITY_MIN = 0;
+
+        internal void IncrementQuality(int incrementNumber = 1)
+        {
+            this.Quality = Math.Min(this.Quality + 1, QUALITY_MAX);
+        }
+        internal void DecrementQuality(int decrementNumber = 1)
+        {
+            this.Quality = Math.Max(this.Quality - decrementNumber, QUALITY_MIN);
+        }
     }
 }
