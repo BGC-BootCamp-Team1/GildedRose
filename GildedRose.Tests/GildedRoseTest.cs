5dd9d27ec8e196594c7a3a022547843837364fee
Anyan Zhao <AZhao4@slb.com>
2024-10-30 19:17:32 +0800

    test: compare benchmark file and result file

--- GildedRose.Tests/GildedRoseTest.cs
+++ GildedRose.Tests/GildedRoseTest.cs
@@ -7,13 +7,16 @@ namespace GildedRoseTest
         [Fact]
         public void TestConsoleOutputString()
         {
-            string expectedOutput = File.ReadAllText("GildedRoseOutput.txt");
+            string pathToExpectedFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\GildedRose.benchmark.txt"); 
+            string pathToResultFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\GildedRose.result.txt"); 
+
+            string expectedOutput = File.ReadAllText(pathToExpectedFile);
             expectedOutput = expectedOutput.Replace("\r\n", "\n").Replace("\n", Environment.NewLine);
             
             string outputStr = Program.GenerateGildedRoseConsoleOutput();
             outputStr = outputStr.Replace("\r\n", "\n").Replace("\n", Environment.NewLine);
+            File.WriteAllText(pathToResultFile, outputStr);
 
-            //Assert.Equal(10516, outputStr.Length);
             Assert.Equal(expectedOutput, outputStr);
         }
 
